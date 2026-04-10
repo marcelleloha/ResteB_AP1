3c558a37bfcd121de717e46443d3fa21e0b3cf59
MARCELLE LOHANE GONCALVES GANIMO <202402726056@alunos.ibmec.edu.br>
2026-04-09 22:01:19 -0300

    subindo versao aula

---
+++ catalogo/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for catalogo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.http import JsonResponse
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+def healthcheck(_request):
+    return JsonResponse({'status': 'ok'})
+
+urlpatterns = [
+    path('', healthcheck),  # Health check do EB retorna 200
+    path('admin/', admin.site.urls),
+    path('api/', include('produtos.urls')),
+]
+
+# Configuração para servir arquivos de mídia em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
